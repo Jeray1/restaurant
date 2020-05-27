d9c855ce52b86e0051ac6095beadebe5f2c691f7
Beñat Madariaga <46680153+Bernyato@users.noreply.github.com>
2020-05-27 14:02:50 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 Proyecto de restaurante de juanma
 
 A tope de pawa!!!!
-
-Vamos equipo!! Todos juntos podemos avanzar!!
--- Ventanas/Principal.java
+++ Ventanas/Principal.java
@@ -1,5 +1,10 @@
 package Ventanas;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JOptionPane;
+
 import BD.BDRestaurante;
 import Clases.Camarero;
 import Enumeraciones.Contratos;
@@ -7,6 +12,8 @@ import Enumeraciones.Contratos;
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		int anno, mes, dia, hora, minutos;
 		
 		BDRestaurante bd1 = new BDRestaurante();
 		bd1.conectar();
@@ -26,6 +33,33 @@ public class Principal {
 		bd1.ocuparMesa(25);
 		
 		bd1.librarMesa(7);
+		
+		// hora de 24h - De 0 a 23
+		
+		anno = Integer.parseInt(JOptionPane.showInputDialog(null, "Año"));
+		mes = Integer.parseInt(JOptionPane.showInputDialog(null, "Mes"));
+		dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Dia"));
+		hora = Integer.parseInt(JOptionPane.showInputDialog(null, "Hora"));
+		minutos = Integer.parseInt(JOptionPane.showInputDialog(null, "Minutos"));
+		
+		reservarFecha(anno, mes, dia, hora, minutos);
+	}
+
+	private static Calendar reservarFecha(int anno, int mes, int dia, int hora, int minutos) {
+		Calendar fecha = Calendar.getInstance();
+
+		fecha.set(Calendar.YEAR, anno);
+		// Month. 0 is January, 11 is November
+		fecha.set(Calendar.MONTH, (mes-1));
+		fecha.set(Calendar.DAY_OF_MONTH, dia);
+		fecha.set(Calendar.HOUR_OF_DAY, hora);
+
+		fecha.set(Calendar.MINUTE, minutos);
+
+
+		System.out.println("Fecha reserva: " + fecha.getTime());
+		return fecha;
+		
 	}
 
 }
