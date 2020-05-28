840592fcbbc8b63388b99d246843fc6f8429a274
Beñat Madariaga <46680153+Bernyato@users.noreply.github.com>
2020-05-28 11:18:49 +0200

    Sacar hora reserva

--- Ventanas/Principal.java
+++ Ventanas/Principal.java
@@ -13,7 +13,6 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		int anno, mes, dia, hora, minutos;
 		
 		BDRestaurante bd1 = new BDRestaurante();
 		bd1.conectar();
@@ -22,10 +21,10 @@ public class Principal {
 		m1.setVisible(true);
 		Camarero unCamarero;
 		unCamarero = new Camarero("Cam3", "1DAW3", "cam3@gmail.com", "Fernando", "Surero", "987654321", "9876598765P", Contratos.Temporal, (float)900.99, "Mesa 3 Sucia", "M", "9:00-19:00", 1, 1, (float)11.3);
-		bd1.añadirCamarero(unCamarero);
+		bd1.aÃ±adirCamarero(unCamarero);
 
 		for (int i = 0; i < 40; i++) {
-			bd1.añadirMesa();
+			bd1.aÃ±adirMesa();
 			
 		}
 		
@@ -36,28 +35,31 @@ public class Principal {
 		
 		// hora de 24h - De 0 a 23
 		
-		anno = Integer.parseInt(JOptionPane.showInputDialog(null, "Año"));
+		
+		
+		reservarFecha();
+	}
+
+	private static Calendar reservarFecha() {
+		
+
+		int anno, mes, dia, hora, minutos;
+		
+		anno = Integer.parseInt(JOptionPane.showInputDialog(null, "AÃ±o"));
 		mes = Integer.parseInt(JOptionPane.showInputDialog(null, "Mes"));
 		dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Dia"));
 		hora = Integer.parseInt(JOptionPane.showInputDialog(null, "Hora"));
 		minutos = Integer.parseInt(JOptionPane.showInputDialog(null, "Minutos"));
 		
-		reservarFecha(anno, mes, dia, hora, minutos);
-	}
-
-	private static Calendar reservarFecha(int anno, int mes, int dia, int hora, int minutos) {
 		Calendar fecha = Calendar.getInstance();
 
 		fecha.set(Calendar.YEAR, anno);
-		// Month. 0 is January, 11 is November
 		fecha.set(Calendar.MONTH, (mes-1));
 		fecha.set(Calendar.DAY_OF_MONTH, dia);
 		fecha.set(Calendar.HOUR_OF_DAY, hora);
-
 		fecha.set(Calendar.MINUTE, minutos);
-
-
-		System.out.println("Fecha reserva: " + fecha.getTime());
+		
+		
 		return fecha;
 		
 	}
